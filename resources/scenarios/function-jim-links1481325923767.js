ccae19c126e769d10952356de61a821347923200
Jaime Flores <jaimelopesflores@gmail.com>
2016-12-09 21:37:56 -0200

    first commit

---
+++ resources/scenarios/function-jim-links1481325923767.js
@@ -0,0 +1,21 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "ad6e0ec9-04a3-4563-855b-cb97b74052a2" : {
+      "Rectangle_4" : [
+        "e8db555d-83de-4f54-9a92-9cd2ffde7ea2"
+      ]
+    },
+    "e8db555d-83de-4f54-9a92-9cd2ffde7ea2" : {
+      "Rectangle_4" : [
+        "03d9ec7b-e313-4c42-8480-40a7cfe9c0eb"
+      ]
+    },
+    "03d9ec7b-e313-4c42-8480-40a7cfe9c0eb" : {
+    },
+    "1ea7ee48-8e53-41e1-bc6f-5e7c27c40a0d" : {
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
