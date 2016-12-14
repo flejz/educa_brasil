33dacdf9c213363ef810f7f5fc876c8f3a9abd05
Jaime Flores <jaimelopesflores@gmail.com>
2016-12-14 13:08:14 -0200

    Adequação

---
+++ resources/prototype-1481727957853.js
@@ -0,0 +1,396 @@
+(function(window, undefined) {
+  var dictionary = {
+    "ad6e0ec9-04a3-4563-855b-cb97b74052a2": "Login",
+    "e8db555d-83de-4f54-9a92-9cd2ffde7ea2": "Classes",
+    "71389cd5-6a30-4a44-921a-4a15fe9caf6c": "Disciplines",
+    "03d9ec7b-e313-4c42-8480-40a7cfe9c0eb": "Streaming",
+    "87db3cf7-6bd4-40c3-b29c-45680fb11462": "960 grid - 16 columns",
+    "e5f958a4-53ae-426e-8c05-2f7d8e00b762": "960 grid - 12 columns",
+    "f39803f7-df02-4169-93eb-7547fb8c961a": "Template 1",
+    "c6c716c7-1186-4ff9-8902-e9f54335788d": "Page Template",
+    "f4aa0d5f-02ed-4d94-b92c-63db03d0263e": "Header",
+    "1ea7ee48-8e53-41e1-bc6f-5e7c27c40a0d": "Footer",
+    "bb8abf58-f55e-472d-af05-a7d1bb0cc014": "default"
+  };
+
+  var uriRE = /^(\/#)?(screens|templates|masters|scenarios)\/(.*)(\.html)?/;
+  window.lookUpURL = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, url;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      url = folder + "/" + canvas;
+    }
+    return url;
+  };
+
+  window.lookUpName = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, canvasName;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      canvasName = dictionary[canvas];
+    }
+    return canvasName;
+  };
+})(window);jQuery("#simulation")
+  .on("click", ".s-ad6e0ec9-04a3-4563-855b-cb97b74052a2 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Rectangle_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/71389cd5-6a30-4a44-921a-4a15fe9caf6c",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseenter dragenter", ".s-ad6e0ec9-04a3-4563-855b-cb97b74052a2 .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-ad6e0ec9-04a3-4563-855b-cb97b74052a2 #s-Rectangle_4": {
+                      "attributes": {
+                        "background-color": "#67971A",
+                        "background-image": "none",
+                        "box-shadow": "none",
+                        "text-shadow": "none"
+                      }
+                    }
+                  },{
+                    "#s-ad6e0ec9-04a3-4563-855b-cb97b74052a2 #s-Rectangle_4": {
+                      "attributes-ie": {
+                        "-pie-background": "#67971A",
+                        "-pie-poll": "false"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-ad6e0ec9-04a3-4563-855b-cb97b74052a2 .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4")) {
+      jEvent.undoCases(jFirer);
+    }
+  });jQuery("#simulation")
+  .on("click", ".s-e8db555d-83de-4f54-9a92-9cd2ffde7ea2 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Rectangle_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/03d9ec7b-e313-4c42-8480-40a7cfe9c0eb",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseenter dragenter", ".s-e8db555d-83de-4f54-9a92-9cd2ffde7ea2 .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-e8db555d-83de-4f54-9a92-9cd2ffde7ea2 #s-Rectangle_4": {
+                      "attributes": {
+                        "background-color": "#67971A",
+                        "background-image": "none",
+                        "box-shadow": "none",
+                        "text-shadow": "none"
+                      }
+                    }
+                  },{
+                    "#s-e8db555d-83de-4f54-9a92-9cd2ffde7ea2 #s-Rectangle_4": {
+                      "attributes-ie": {
+                        "-pie-background": "#67971A",
+                        "-pie-poll": "false"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-e8db555d-83de-4f54-9a92-9cd2ffde7ea2 .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4")) {
+      jEvent.undoCases(jFirer);
+    }
+  });jQuery("#simulation")
+  .on("click", ".s-71389cd5-6a30-4a44-921a-4a15fe9caf6c .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e8db555d-83de-4f54-9a92-9cd2ffde7ea2",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e8db555d-83de-4f54-9a92-9cd2ffde7ea2",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e8db555d-83de-4f54-9a92-9cd2ffde7ea2",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Image_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e8db555d-83de-4f54-9a92-9cd2ffde7ea2",
+                    "transition": "fade"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });jQuery("#simulation")
+  .on("mouseenter dragenter", ".s-03d9ec7b-e313-4c42-8480-40a7cfe9c0eb .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-03d9ec7b-e313-4c42-8480-40a7cfe9c0eb #s-Rectangle_4": {
+                      "attributes": {
+                        "background-color": "#67971A",
+                        "background-image": "none",
+                        "box-shadow": "none",
+                        "text-shadow": "none"
+                      }
+                    }
+                  },{
+                    "#s-03d9ec7b-e313-4c42-8480-40a7cfe9c0eb #s-Rectangle_4": {
+                      "attributes-ie": {
+                        "-pie-background": "#67971A",
+                        "-pie-poll": "false"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-03d9ec7b-e313-4c42-8480-40a7cfe9c0eb .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4")) {
+      jEvent.undoCases(jFirer);
+    }
+  });jQuery("#simulation")
+  .on("click", ".m-1ea7ee48-8e53-41e1-bc6f-5e7c27c40a0d .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#m-1ea7ee48-Image_map_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "isexternal": true,
+                    "target": "http://www.yastech.ca/"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
---
+++ resources/scenarios/function-jim-links1481727957853.js
@@ -0,0 +1,35 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "ad6e0ec9-04a3-4563-855b-cb97b74052a2" : {
+      "Rectangle_4" : [
+        "71389cd5-6a30-4a44-921a-4a15fe9caf6c"
+      ]
+    },
+    "e8db555d-83de-4f54-9a92-9cd2ffde7ea2" : {
+      "Rectangle_4" : [
+        "03d9ec7b-e313-4c42-8480-40a7cfe9c0eb"
+      ]
+    },
+    "71389cd5-6a30-4a44-921a-4a15fe9caf6c" : {
+      "Image_1" : [
+        "e8db555d-83de-4f54-9a92-9cd2ffde7ea2"
+      ],
+      "Image_3" : [
+        "e8db555d-83de-4f54-9a92-9cd2ffde7ea2"
+      ],
+      "Image_5" : [
+        "e8db555d-83de-4f54-9a92-9cd2ffde7ea2"
+      ],
+      "Image_7" : [
+        "e8db555d-83de-4f54-9a92-9cd2ffde7ea2"
+      ]
+    },
+    "03d9ec7b-e313-4c42-8480-40a7cfe9c0eb" : {
+    },
+    "1ea7ee48-8e53-41e1-bc6f-5e7c27c40a0d" : {
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
